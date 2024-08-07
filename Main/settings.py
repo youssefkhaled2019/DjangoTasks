da7e6c8579c2fb50aa8e1c694cd16031f6c1dd68
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2024-08-07 21:57:29 +0200

    update env

--- Main/settings.py
+++ Main/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
